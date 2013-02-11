273f5d72c27b948abfd92dea3e6c58e8b0439f43
ILoGM <a.a.mikka@gmail.com>
2013-02-11 23:15:17 +0400

    Refactor ban_user_email action: add comment to params

--- app/mailers/board_mailer.rb
+++ app/mailers/board_mailer.rb
@@ -1,7 +1,8 @@
 class BoardMailer < ActionMailer::Base
   default from: "admin@board.com"
 
-  def user_banned_email(user)
+  def user_banned_email(user, comment)
+    @params = {comment: comment}
     mail(:to => user.email, :subject => I18n.t('board_mailer.user_banned_email.user_banned_title'))
   end
 
