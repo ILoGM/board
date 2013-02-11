3b9672fc101e404257366ecf69491ded706643de
Alexey Mikka <a.a.mikka@gmail.com>
2013-02-11 11:32:20 +0400

    Update migration version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130207122545) do
+ActiveRecord::Schema.define(:version => 20130207123134) do
 
   create_table "admin_comments", :force => true do |t|
     t.string   "action_type"
